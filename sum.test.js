2ebd30366b4cf708a802e5fcb5eb3c5a05d754da
Mazen Mohamed <104158971+mazenfteha@users.noreply.github.com>
2023-05-15 14:39:43 +0300

    using matchers methods

--- sum.test.js
+++ sum.test.js
@@ -12,4 +12,52 @@ describe("example tests", () => {
     })
 })
 
+//Truthiness
+describe("truthy or falsy", () => {
+    it("null", ()=> {
+        const n = null;
+        expect(n).toBeFalsy()
+        expect(n).not.toBeTruthy()
+        expect(n).toBeNull()
+        expect(n).not.toBeUndefined();
+    })
 
+    it("zero", () => {
+        const z = 0;
+        expect(z).not.toBeNull()
+        expect(z).not.toBeUndefined()
+        expect(z).not.toBeTruthy()
+        expect(z).toBeFalsy()
+    })
+})
+
+//numbers
+describe("numbers", () => {
+    it("two plus two", () => {
+        const value = 2 + 2;
+        expect(value).toBe(4)
+        expect(value).toEqual(4)
+        expect(value).toBeGreaterThan(3)
+        expect(value).toBeLessThan(5);
+    })
+})
+
+//strings
+describe("strings with regular expressions", () => {
+    it("there is no I in team", () => {
+        expect("team").not.toMatch(/I/)
+    })
+})
+
+//arrays
+describe("Metro Boomin", ()=>{
+    const metroIntros = [
+        'metroooo',
+        'metro boomin wants some more nigga',
+        'metro boomin make it boom',
+        'if yong metro dosent trust you i will shot you',
+    ]
+    it('metroo introductions has metroooo on it', () => {
+        expect(metroIntros).toContain('metroooo')
+    })
+})
